3ac278f05fe7c24bf06200e0ad26b3af0e3cb73f
MysticAakash07 <rahulsagar460@gmail.com>
2025-03-31 15:16:11 +0530

    Add & Style NavBar

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -3,7 +3,29 @@ import SideBar from "./SideBar";
 import NavBar from "./NavBar";
 import Body from "./Body";
 import Footer from "./Footer";
+import { useEffect } from "react";
+import { useStateProvider } from "../utils/StateProvider";
+import axios from "axios";
+import { reducerCases } from "../utils/Constants";
 export default function Spotify() {
+	const [{ token }, dispatch] = useStateProvider();
+
+	useEffect(() => {
+		const getUserInfo = async () => {
+			const { data } = await axios.get("https://api.spotify.com/v1/me", {
+				headers: {
+					Authorization: "Bearer " + token,
+					"Content-Type": "application/json",
+				},
+			});
+			const userInfo = {
+				userId: data.id,
+				userName: data.display_name,
+			};
+			dispatch({ type: reducerCases.SET_USER, userInfo });
+		};
+		getUserInfo();
+	}, [token, dispatch]);
 	return (
 		<Container>
 			<div className="spotify_body">

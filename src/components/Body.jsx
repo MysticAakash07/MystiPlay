7bbe7dc7554dfa96a809efef683119e1bf0198cf
MysticAakash07 <rahulsagar460@gmail.com>
2025-04-15 19:06:29 +0530

    Add Song Selection

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -64,12 +64,42 @@ export default function Body({ headerBackground }) {
 	const playTrack = async (
 		id,
 		name,
-		artist,
+		artists,
 		image,
 		context_uri,
 		track_number
 	) => {
-		
+		const response = await axios.put(
+			`https://api.spotify.com/v1/me/player/play/`,
+			{
+				context_uri,
+				offset: {
+					position: track_number - 1,
+				},
+				position_ms: 0,
+			},
+			{
+				headers: {
+					Authorization: "Bearer " + token,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		if (response.status === 204) {
+			const currentlyPlaying = {
+				id,
+				name,
+				artists,
+				image,
+			};
+			dispatch({ type: reducerCases.SET_PLAYING, currentlyPlaying });
+			dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
+		} else {
+			dispatch({
+				type: reducerCases.SET_PLAYER_STATE,
+				playerState: true,
+			});
+		}
 	};
 	return (
 		<Container headerBackground={headerBackground}>
